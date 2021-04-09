d5da6d33c0c76f68c7836ee89fdbe8102c9b5a1f
Imran <muhdimran1810@gmail.com>
2021-04-09 18:24:34 +0800

    Init commit

---
+++ TranslationClient/src/application.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class application {
+	public static void main(String[] args){
+		Frame serverFrame = new Frame();
+		serverFrame.setVisible(true);
+	}
+}
---
+++ TranslationServer/src/translate.java
@@ -0,0 +1,7 @@
+
+public class translate {
+	public static String trl(String msg) {
+		
+		return "k";
+	}
+}
